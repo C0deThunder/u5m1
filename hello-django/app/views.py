37440e61114a2202bdef018d9ea344d37e996986
C0deThunder <keenumw2005@gmail.com>
2023-10-27 10:43:02 -0500

    type ann

--- hello-django/app/views.py
+++ hello-django/app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.http.request import HttpRequest
 
-def hello_view(request):
+def hello_view(request:HttpRequest) -> HttpResponse:
     return HttpResponse("Hello World!")
